61998eedf0af937bacfa1fe97951c096795349d0
Nitish <nk6071775@gmail.com>
2026-05-16 14:04:57 +0530

    updated code

--- src/screens/teachers_screen.py
+++ src/screens/teachers_screen.py
@@ -161,7 +161,7 @@ def teacher_tab_take_attendace():
                     current_timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
 
-                    for node in enrolled_students(type='tertiary',vertical_alignment='center'):
+                    for node in enrolled_students(type='tertiary'):
                         student = node['students']
                         sources = all_detected_ids.get(int(student['student_id']), [])
                         is_present= len(sources) > 0
